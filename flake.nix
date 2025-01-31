cea3e733659bcce8cdf3a22e02302e4ad8b5105e
cristianoliveira <contato@cristianoliveira.com.br>
2025-01-31 19:58:00 +0100

    chore: adds nix flakes

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "sway-setter cli";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+        };
+    });
+}
