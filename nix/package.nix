f205b61a30ba1a0f4c6d3c73362e89fa4150a689
cristianoliveira <contato@cristianoliveira.com.br>
2025-02-02 19:56:45 +0100

    chore(nix): bump v0.0.2

--- nix/package.nix
+++ nix/package.nix
@@ -2,14 +2,14 @@
   pkgs.buildGoModule rec {
     # name of our derivation
     name = "sway-setter";
-    version = "v0.0.1";
+    version = "v0.0.2";
 
     # sources that will be used for our derivation.
     src = pkgs.fetchFromGitHub {
       owner = "cristianoliveira";
       repo = "sway-setter";
       rev = version;
-      sha256 = "sha256-U1az7xUG2SF4DGn6sXO83wiWNLpuiLvHeHHurJcw9Ls=";
+      sha256 = "sha256-TQAcKtIid1+zP6dRixcuH6jZZHko1+ujZH2Kya+Snq8=";
     };
 
     vendorHash = "sha256-1pCoyPjokM+Qm+mbNQLCOmVVqwHDUxJ10RyvLV/becQ=";
